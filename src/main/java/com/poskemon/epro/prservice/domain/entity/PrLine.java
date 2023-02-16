f2ac714b1d6107f56f8448fa1ed287f319f9c038
isohyeon <isohyeon0816@gmail.com>
2023-02-16 21:48:49 +0900

    chore: 코드 정리

--- src/main/java/com/poskemon/epro/prservice/domain/entity/PrLine.java
+++ src/main/java/com/poskemon/epro/prservice/domain/entity/PrLine.java
@@ -1,7 +1,6 @@
 package com.poskemon.epro.prservice.domain.entity;
 
 import java.time.LocalDate;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
