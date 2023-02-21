09c45cc84654ca848d449649ce3e01611531cfdc
isohyeon <isohyeon0816@gmail.com>
2023-02-21 11:21:24 +0900

    fix: 구매단위편성 조건 조회 수정
    응답 데이터 react 화면에 맞게 변경

--- src/main/java/com/poskemon/epro/prservice/domain/dto/PurchaseUnitRes.java
+++ src/main/java/com/poskemon/epro/prservice/domain/dto/PurchaseUnitRes.java
@@ -16,9 +16,13 @@ public class PurchaseUnitRes {
     private String prStatus; // 진행상태 (승인요청, 승인완료)
     private Long rfqNo;
     private Long elapsedDate; // 현재날짜 - 승인날짜
-    private Item item;
+    private String category;
+    private String itemDescription;
+    private String spec;
+    private String uom;
     private String prNo;
     private Long prLine;
+    private Long unitPrice;
     private Long prQuantity;
     private Long buyerNo;
     private String buyerName;
@@ -31,9 +35,13 @@ public class PurchaseUnitRes {
     public PurchaseUnitRes(PrLine prLine) {
         this.prStatus = prLine.getPrHeader().getPrStatus();
         this.rfqNo = prLine.getRfqNo();
-        this.item = prLine.getItem();
+        this.category = prLine.getItem().getCategory();
+        this.itemDescription = prLine.getItem().getItemDescription();
+        this.spec = prLine.getItem().getSpec();
+        this.uom = prLine.getItem().getUom();
         this.prNo = prLine.getPrHeader().getPrNo();
         this.prLine = prLine.getPrLine();
+        this.unitPrice = prLine.getUnitPrice();
         this.prQuantity = prLine.getPrQuantity();
         this.buyerNo = prLine.getBuyerNo();
         this.requesterNo = prLine.getPrHeader().getRequesterNo();
